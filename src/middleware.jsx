efb5cb82eeb543edd8b648846a3bbe43027a33ce
akiran1sei <nakamori.1224@gmail.com>
2024-01-16 16:44:32 +0900

    不要なコードの削除

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -3,16 +3,14 @@
 import { NextResponse } from "next/server";
 import { jwtVerify } from "jose";
 export async function middleware(request) {
-  const token =
-    "eyJhbGciOiJIUzI1NiJ9.eyJlbWFpbCI6Im5ha2Ftb3JpLjEyMjRAZ21haWwuY29tIiwiZXhwIjoxNzA1Mzg2OTc3fQ.KNEyQ9Bw6u9e5lsZ9zw8F-qYQ4esphE8GlzT1yYLNVg";
-  // const token = await request.headers.get("Authorization")?.split(" ")[1];
+  const token = await request.headers.get("Authorization")?.split(" ")[1];
   if (!token) {
     return NextResponse.json({ message: "トークンがありません" });
   }
   try {
     const secretKey = new TextEncoder().encode("TastingNote");
     const decodedJwt = await jwtVerify(token, secretKey);
-    // console.log("decodedJwt:", decodedJwt);
+
     return NextResponse.next();
   } catch (error) {
     return NextResponse.json({
