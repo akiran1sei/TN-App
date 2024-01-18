23e32043f79241dec087eeec8bfb04a33ca552ac
akiran1sei <nakamori.1224@gmail.com>
2024-01-18 15:35:56 +0900

    BeansSchema項目追加「email」

--- src/utils/schemaModels.js
+++ src/utils/schemaModels.js
@@ -7,6 +7,7 @@ const Schema = mongoose.Schema;
 const BeansSchema = new Schema(
   {
     username: String,
+    email: String,
     coffee: String,
     roast: String,
     roastMessage: String,
@@ -40,7 +41,6 @@ const BeansSchema = new Schema(
     result: String,
     total: String,
     date: String,
-    email: String,
   },
   { timestamps: true }
 );
