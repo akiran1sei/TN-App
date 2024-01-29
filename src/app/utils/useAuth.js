b0bdd139dd7bb71caf63ac8ee61709fc456c6d06
akiran1sei <nakamori.1224@gmail.com>
2024-01-29 13:33:18 +0900

    強制ループの確認につきコメントアウトを削除し、元に戻した

--- src/app/utils/useAuth.js
+++ src/app/utils/useAuth.js
@@ -10,14 +10,14 @@ const useAuth = () => {
     const checkToken = async () => {
       const token = localStorage.getItem("token");
       if (!token) {
-        // router.push("/");
+        router.push("/");
       }
       try {
         const secretKey = new TextEncoder().encode("TastingNote");
         const decodedJwt = await jwtVerify(token, secretKey);
         setLoginUserEmail(decodedJwt.payload.email);
       } catch (error) {
-        // router.push("/");
+        router.push("/");
       }
     };
     checkToken();
