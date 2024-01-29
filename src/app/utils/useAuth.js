0412c364b62fc7042eab694ef74e4367b871c935
akiran1sei <nakamori.1224@gmail.com>
2024-01-29 15:29:30 +0900

    useAuth設定変更

--- src/app/utils/useAuth.js
+++ src/app/utils/useAuth.js
@@ -5,19 +5,24 @@ import { useState, useEffect } from "react";
 
 const useAuth = () => {
   const [loginUserEmail, setLoginUserEmail] = useState("");
+  const [hasNavigated, setHasNavigated] = useState(false);
   const router = useRouter();
   useEffect(() => {
     const checkToken = async () => {
       const token = localStorage.getItem("token");
-      if (!token) {
+      if (!token || !hasNavigated) {
         router.push("/pages/user/login");
+        setHasNavigated(true);
       }
       try {
         const secretKey = new TextEncoder().encode("TastingNote");
         const decodedJwt = await jwtVerify(token, secretKey);
         setLoginUserEmail(decodedJwt.payload.email);
       } catch (error) {
-        router.push("/pages/user/login");
+        if (!hasNavigated) {
+          router.push("/pages/user/login");
+          setHasNavigated(true);
+        }
       }
     };
     checkToken();
