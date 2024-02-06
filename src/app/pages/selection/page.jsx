0b890140a2ceeaad8c231178699b93303137a343
akiran1sei <nakamori.1224@gmail.com>
2024-02-06 12:47:32 +0900

    セレクションページの改善（table表示に切り替えに伴うコーディング）

--- src/app/pages/selection/page.jsx
+++ src/app/pages/selection/page.jsx
@@ -1,7 +1,7 @@
 import Head from "next/head";
 // import { SelectionForm } from "../../components/molecules/Selection/SelectionForm-simple";
-import { SelectionForm } from "../../components/molecules/Selection/SelectionForm";
-
+// import { SelectionForm } from "../../components/molecules/Selection/SelectionForm";
+import { SelectionForm } from "@/app/components/molecules/Selection/SelectionForm-table";
 import { revalidatePath } from "next/cache";
 const SelectionPage = async () => {
   const AppUrl = `https://netlify--courageous-creponne-2fa598.netlify.app`;
