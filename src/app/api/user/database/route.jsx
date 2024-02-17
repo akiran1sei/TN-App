86ce509650dc602fe17eb51e2dba9ab67c3001fa
akiran1sei <nakamori.1224@gmail.com>
2024-02-17 15:41:52 +0900

    専用データベース作成ファイル群のテンプレート作成

---
+++ src/app/api/user/database/route.jsx
@@ -0,0 +1,3 @@
+const { NextResponse } = require("next/server");
+import connectDB from "@/app/utils/database";
+export async function POST() {}
---
+++ src/app/user/database/page.jsx
@@ -0,0 +1,10 @@
+import { DBCreatePage } from "@/app/components/molecules/DataBase/DBCreate";
+
+const DBCreate = () => {
+  return (
+    <>
+      <DBCreatePage />
+    </>
+  );
+};
+export default DBCreate;
