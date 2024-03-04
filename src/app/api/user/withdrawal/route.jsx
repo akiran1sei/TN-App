72254825d8a1ad652350cecde813f478c583fb3e
akiran1sei <nakamori.1224@gmail.com>
2024-03-04 10:51:09 +0900

    account削除時と同時に削除ユーザーが作成したデータも削除するコーディング

--- src/app/api/user/withdrawal/route.jsx
+++ src/app/api/user/withdrawal/route.jsx
@@ -1,12 +1,14 @@
 const { NextResponse } = require("next/server");
 import connectDB from "@/app/utils/database";
-import { UserModel } from "@/app/utils/schemaModels";
+import { BeansModel, UserModel } from "@/app/utils/schemaModels";
 export async function POST(request) {
   const body = await request.json();
-
+  console.log("body:", body.email, body.id);
   try {
     await connectDB();
-    await UserModel.deleteOne({ _id: body.id });
+    const DeleteUser = await UserModel.findOne({ _id: body.id });
+    await UserModel.deleteOne({ _id: DeleteUser.id });
+    await BeansModel.deleteMany({ email: DeleteUser.email });
     return NextResponse.json({
       message: "ユーザー削除成功",
 
