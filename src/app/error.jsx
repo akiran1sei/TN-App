030e0f93bf06ee35d696cc31abfb5da19ef34d90
akiran1sei <nakamori.1224@gmail.com>
2024-03-06 15:47:36 +0900

    errorページコーディング

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -1,14 +1,36 @@
-"use client";
+"use client"; // Error components must be Client Components
+import styles from "@/app/styles/Home.module.css";
+import { useEffect } from "react";
+import Link from "next/link";
+export default function Error({ error, reset }) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
 
-export default function GlobalError({ error, reset }) {
   return (
-    <html>
-      <body>
-        <h2>Something went wrong!</h2>
-        <button type="button" onClick={() => reset()}>
+    <div className={styles.error_page}>
+      <h2 className={styles.error_title}>
+        Something
+        <br className={styles.error_break} /> went
+        <br className={styles.error_break} /> wrong!
+      </h2>
+      <div className={styles.error_BtnBox}>
+        <button
+          onClick={
+            // Attempt to recover by trying to re-render the segment
+            () => reset()
+          }
+          className={styles.error_button}
+        >
           Try again
         </button>
-      </body>
-    </html>
+        <button type="button" className={styles.error_button}>
+          <Link href={"/"} scroll={false} passHref>
+            Top
+          </Link>
+        </button>
+      </div>
+    </div>
   );
 }
